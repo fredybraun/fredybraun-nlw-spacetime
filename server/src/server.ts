560d3c9d721a883066dd8b571973fac49f51b1e2
Fredy Braun <fredy.braun@gmail.com>
2023-05-16 23:10:25 -0300

    install next and dependences

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,17 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
+app.get('/hello', async () => {
+  const users = await prisma.user.findMany()
 
-app.get('/hello', () => {
-    return 'Hello word'
+  return users
 })
-
 app.listen({
     port: 3333,
-}).then(() => {
+  })
+  .then(() => {
     console.log('👌 HTTP server running on http://localhost:3333')
-})
+  })
